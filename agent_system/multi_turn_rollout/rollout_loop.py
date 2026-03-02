e0bfd6afc90c89b123c18387b0dea89d99afa859
Toyoid <hanxiao.li@csu.edu.cn>
2026-03-02 23:46:11 +0800

    test judge

--- agent_system/multi_turn_rollout/rollout_loop.py
+++ agent_system/multi_turn_rollout/rollout_loop.py
@@ -843,6 +843,7 @@ class TrajectoryCollector:
                     critique=critique,
                     template=self.config.judge_model.template_name,
                 )
+                print(judge_chat)
                 all_judge_prompts.append(judge_chat)
                 all_judge_imgs.append(judge_images[item] if judge_images is not None else None)
 
