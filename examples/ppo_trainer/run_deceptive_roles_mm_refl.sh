530c83e029d78092436109389fec410b4f2dfade
Hanxiao Li <li.hahn3@gmail.com>
2026-03-12 22:59:09 +0800

    simple sync

--- examples/ppo_trainer/run_deceptive_roles_mm_refl.sh
+++ examples/ppo_trainer/run_deceptive_roles_mm_refl.sh
@@ -109,7 +109,7 @@ python3 -m verl.trainer.main_ppo \
     algorithm.reflection.prompt_style=critique_only \
     algorithm.reflection.monitor_rollout_n=1 \
     algorithm.reflection.min_trust_penalty=0.25 \
-    algorithm.reflection.debug_stop_after=none \
+    algorithm.reflection.debug_stop_after='re-pairing' \
     env.env_name=ReasonChat \
     env.seed=0 \
     env.max_steps=1 \
@@ -123,7 +123,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.rollout_data_dir=auto \
     trainer.log_distributions=True \
     trainer.project_name='verl_deceptive_roles' \
-    trainer.experiment_name='grpo_qwen7b_refl_test' \
+    trainer.experiment_name='grpo_qwen7b_refl' \
     trainer.n_gpus_per_node=4 \
     trainer.nnodes=1 \
     trainer.n_gpus_per_node_monitor=4 \
